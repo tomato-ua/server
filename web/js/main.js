ff61165e0a8bcd7ab74e57b04ce2cc5cf418760b
Vitaliy Pitvalo <vitaliy@ekreative.com>
2016-03-12 04:06:52 +0200

    move js to file

---
+++ web/js/main.js
@@ -0,0 +1,65 @@
+/**
+ * Created by vitaliy on 3/12/16.
+ */
+
+
+$(function () {
+
+    var deputats = [];
+    $('li[data-href]').click(function (e) {
+        var player = $('#player');
+        console.info(e);
+        player.prop('src', $(e.target).attr('data-href'));
+    });
+
+
+    var substringMatcher = function (strs) {
+        return function findMatches(q, cb) {
+            var matches= [], substringRegex;
+
+            substrRegex = new RegExp(q, 'i');
+
+            $.each(strs, function (i, str) {
+                if (substrRegex.test(str.full)) {
+                    matches.push(str.full);
+
+
+                }
+            });
+
+            cb(matches);
+        };
+    };
+
+
+    $('.typeahead').typeahead({
+            minLength: 3,
+            highlight: true
+        },
+        {
+            name: 'my-dataset',
+            source: substringMatcher(deputats)
+        });
+
+
+    $.ajax({
+        dataType: "json",
+        url: 'http://cors.io/?u=http://www.chesno.org/persons/json/deputies/8/?format=json',
+        success: function (data) {
+
+            $.each(data,function (i, deputat) {
+
+                var d = {
+                    "shortname": deputat.last_name + ' ' + deputat.first_name[0] + '.' + deputat.second_name[0] + '.',
+                    "full": deputat.last_name + ' ' + deputat.first_name + '.' + deputat.second_name + '.'
+                };
+                deputats.push(d);
+
+            });
+
+            console.log(deputats);
+        }
+    });
+
+
+});
